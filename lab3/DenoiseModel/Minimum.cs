6282eaf73ea4ee6ad49496b1fd77c496181cffb1
danielbulgakov <bulgach09@gmail.com>
2022-10-11 08:58:45 +0300

    unifrom update

---
+++ lab3/DenoiseModel/Minimum.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenoiseModel
+{
+    internal class Minimum
+    {
+        public static Bitmap Execute(Bitmap sourceImage)
+        {
+            int width = sourceImage.Width;
+            int height = sourceImage.Height;
+            Bitmap resImage = new Bitmap(width, height);
+           
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                {
+                    Color Color = CalculateNewPixelColor(sourceImage, x, y);
+                    resImage.SetPixel(x, y, Color);
+
+                }
+
+
+
+            return resImage;
+        }
+
+        public static byte clamp(float value, float min, float max)
+        {
+            return (byte)(Math.Min(Math.Max(min, value), max));
+        }
+
+        public static int clamp(int value, int min, int max)
+        {
+            return (int)(Math.Min(Math.Max(min, value), max));
+        }
+
+        public static Color CalculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            int radius = 1; // радиус матрицы
+            int matrixSize = (1 + 2 * radius) * (1 + 2 * radius);
+
+            byte min = 255;
+
+
+            for (int l = -radius; l <= radius; l++)
+                for (int k = -radius; k <= radius; k++)
+                {
+                    int idX = clamp(x + k, 0, sourceImage.Width - 1);
+                    int idY = clamp(y + l, 0, sourceImage.Height - 1);
+                    Color neighborColor = sourceImage.GetPixel(idX, idY);
+
+                    if (GetBrightness(neighborColor) < min) min = GetBrightness(neighborColor);
+                }
+
+            return Color.FromArgb(min, min,min);
+
+        }
+
+
+
+        public static byte GetBrightness(Color color)
+        {
+            return (byte)(.299 * color.R + .587 * color.G + .114 * color.B);
+        }
+    }
+}
--- lab3/NoiseModels/Uniform.cs
+++ lab3/NoiseModels/Uniform.cs
@@ -1,31 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NoiseModels
 {
+
     internal class Uniform : DistributedModel
     {
+        static int a = 64;
+        static int b = 32;
+
+        static double[] uniform = new double[256];
+        static double sum = 0;
+        static int size;
+        static byte[] noise;
 
         public static Bitmap Execute(Bitmap sourceImage, byte upBorder = 255, byte downBorder = 0)
         {
             Bitmap resImage = new Bitmap(sourceImage);
-
-            const float p = 0.8f;
-            int noisePixels = 0;
-            while (noisePixels < sourceImage.Height * sourceImage.Width * p)
-            {
-                var pos = GetNextRandPixel(sourceImage.Width, sourceImage.Height);
-                noisePixels++;
-                Color color = sourceImage.GetPixel(pos.Item1, pos.Item2);
-                resImage.SetPixel(pos.Item1, pos.Item2, CalculateColor(color, upBorder, downBorder));
-            }
+            size = sourceImage.Width * sourceImage.Height;
+            noise = new byte[size];
 
             
+            for (int y = 0; y < sourceImage.Height; y++)
+                for (int x = 0; x < sourceImage.Width; x++)
+                {
+                    Color color = sourceImage.GetPixel(x, y);
+                    int add = GetBrightness(color) + noise[sourceImage.Width * y + x];
+                    byte v = clamp(GetBrightness(color) + add, 0, 255);
+                    Color newColor = Color.FromArgb(v,v,v);
+                    resImage.SetPixel(x, y, newColor);
+
+                }
+
+
             return resImage;
+        }
+
+        public static byte clamp(float value, float min, float max)
+        {
+            return (byte)(Math.Min(Math.Max(min, value), max));
+        }
+
+
+        private static void ComputeUniform()
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                double step = (double)i;
+                if (step >= a && step <= b)
+                {
+                    uniform[i] = (double)(1 / (b - a));
+                }
+                else
+                {
+                    uniform[i] = 0;
+                }
+                sum += uniform[i];
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                uniform[i] /= sum;
+                uniform[i] *= size;
+                uniform[i] = (int)Math.Floor(uniform[i]);
+            }
+        }
+
+        private static void ComputeNoise()
+        {
+            Random rnd = new Random();
+            int count = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < (int)uniform[i]; j++)
+                {
+                    noise[j + count] = (byte)i;
+                }
+                count += (int)uniform[i];
+            }
+
+            for (int i = 0; i < size - count; i++)
+            {
+                noise[count + i] = 0;
+            }
+
+            noise = noise.OrderBy(x => rnd.Next()).ToArray();
         }
 
         public static Color CalculateColor(Color color, byte up, byte down)
